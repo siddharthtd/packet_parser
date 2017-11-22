12257c85815e9cf9459ab25af004fd546d00739c
Siddharth Thakur <siddharthtd@gmail.com>
2017-11-22 15:06:39 -0800

    added functions to detect packets, display src mac, dest mac and format them. Still errors persist.

--- packet-parser.py
+++ packet-parser.py
@@ -7,25 +7,22 @@ def mac_extracter(data):
     # type: (object) -> object
     raw_dest_mac, raw_src_mac, type = struct.unpack('! 6s 6s H', data[:14])
     type = socket.htons(type)
-    return raw_dest_mac, raw_src_mac, type, data[14:]
+    return raw_dest_mac, raw_src_mac, type, #data[14:]
 
 
-def actual_mac(raw_dest_mac, raw_src_mac):
-    temp_dest_mac = map('{:02x}', format, raw_dest_mac)
-    dest_mac = ':'.join(temp_dest_mac).upper()
-    temp_src_mac = map('{:02x}', format, raw_src_mac)
-    src_mac = ':'.join(temp_src_mac).upper()
-    dest_mac = ':'.join(temp_dest_mac).upper()
-    return dest_mac, src_mac
+def actual_mac(raw_generic_mac):
 
+    a = struct.unpack('!s', len(raw_generic_mac[0:2]))
 
 def main():
+    # type: () -> object
     connect = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
     while True:
         raw_data, address = connect.recvfrom(65536)
-        raw_dest_mac, raw_src_mac, type, data = mac_extracter(raw_data)
-        dest_mac, src_mac = actual_mac(raw_dest_mac, raw_src_mac)
+        raw_dest_mac, raw_src_mac, type = mac_extracter(raw_data)
+        dest_mac = actual_mac(raw_dest_mac)
+        src_mac = actual_mac(raw_src_mac)
         print ('\n{}\n,{}\n,{}\n'.format(dest_mac, src_mac, type))
 
 
