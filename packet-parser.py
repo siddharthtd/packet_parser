f52c8ac9428332fe28d1ee8109fc1597fc33a398
Siddharth Thakur <siddharthtd@gmail.com>
2017-11-22 13:25:05 -0800

    added functions to detect packets, display src mac, dest mac and format them. Still errors persist.

--- packet-parser.py
+++ packet-parser.py
@@ -23,7 +23,7 @@ def main():
     connect = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
     while True:
-        raw_data, address = connect.recvfrom(65535)
+        raw_data, address = connect.recvfrom(65536)
         raw_dest_mac, raw_src_mac, type, data = mac_extracter(raw_data)
         dest_mac, src_mac = actual_mac(raw_dest_mac, raw_src_mac)
         print ('\n{}\n,{}\n,{}\n'.format(dest_mac, src_mac, type))
