2270852c424e845cb929b3846da6f418909d020f
Siddharth Thakur <siddharthtd@gmail.com>
2017-11-23 01:32:11 -0800

    Fixed the errors and extracting Source, Dest MAC and type of Protocol using numbers. Next Development: Name of Protocol

--- packet-parser.py
+++ packet-parser.py
@@ -1,32 +1,28 @@
 import socket
 import struct
 import textwrap
-
+from itertools import chain
 
 def mac_extracter(data):
     raw_dest_mac, raw_src_mac, mac_type = struct.unpack('! 6s 6s H', data[:14])
     mac_type = socket.htons(mac_type)
-    return raw_dest_mac, raw_src_mac, mac_type, #data[14:]
+    return raw_dest_mac, raw_src_mac, mac_type, data[14:]
 
 
 def actual_mac(raw_generic_mac):
-    #print raw_generic_mac
-    a = str(struct.unpack('!B', raw_generic_mac[0]))
-    b = str(struct.unpack('!B', raw_generic_mac[1]))
-    c = str(struct.unpack('!B', raw_generic_mac[2]))
-    d = str(struct.unpack('!B', raw_generic_mac[3]))
-    e = str(struct.unpack('!B', raw_generic_mac[4]))
-    f = str(struct.unpack('!B', raw_generic_mac[5]))
+    bundle = map('{:02x}'.format, (ord(x) for x in raw_generic_mac))
+    mac_addr = ':'.join(bundle).upper()
+    return mac_addr
 
 def main():
     connect = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
     while True:
         raw_data, address = connect.recvfrom(65536)
-        raw_dest_mac, raw_src_mac, mac_type = mac_extracter(raw_data)
-        dest_mac = actual_mac(str(raw_dest_mac))
+        raw_dest_mac, raw_src_mac, mac_type, pckt = mac_extracter(raw_data)
+        dest_mac = actual_mac(raw_dest_mac)
         src_mac = actual_mac(raw_src_mac)
-        #print ('\n{}\n,{}\n,{}\n'.format(dest_mac, src_mac, type))
+        print ('\nDestination MAC:{}\nSource MAC:{}\nProtocol:{}\n'.format(dest_mac, src_mac, mac_type))
 
 
 main()
