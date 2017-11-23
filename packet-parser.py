0860a96c8d6a421280e5ec873edf4e71208803b0
Siddharth Thakur <siddharthtd@gmail.com>
2017-11-23 02:06:02 -0800

    Protocol Type converted to text using dictionary (experimental)

--- packet-parser.py
+++ packet-parser.py
@@ -8,11 +8,14 @@ def mac_extracter(data):
     mac_type = socket.htons(mac_type)
     return raw_dest_mac, raw_src_mac, mac_type, data[14:]
 
-
 def actual_mac(raw_generic_mac):
     bundle = map('{:02x}'.format, (ord(x) for x in raw_generic_mac))
     mac_addr = ':'.join(bundle).upper()
     return mac_addr
+
+def protocol_identifier(type):
+    dict = {8:'IPv4'}
+    return dict[type]
 
 def main():
     connect = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
@@ -22,7 +25,8 @@ def main():
         raw_dest_mac, raw_src_mac, mac_type, pckt = mac_extracter(raw_data)
         dest_mac = actual_mac(raw_dest_mac)
         src_mac = actual_mac(raw_src_mac)
-        print ('\nDestination MAC:{}\nSource MAC:{}\nProtocol:{}\n'.format(dest_mac, src_mac, mac_type))
+        protocol = protocol_identifier(mac_type)
+        print ('\nDestination MAC:{}\nSource MAC:{}\nProtocol:{}\n'.format(dest_mac, src_mac, protocol))
 
 
 main()
