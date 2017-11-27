48b1e4bb4cd6e564c57b838bd075075176dae7a7
Siddharth Thakurdesai <siddharthtd@gmail.com>
2017-11-27 14:39:17 -0800

    IP header information parsing added.

--- packet-parser.py
+++ packet-parser.py
@@ -1,9 +1,9 @@
 import socket
 import struct
 import textwrap
-from binascii import a2b_uu
+
 def mac_extracter(data):
-    raw_dest_mac, raw_src_mac, mac_type = struct.unpack('! 6s 6s
+    raw_dest_mac, raw_src_mac, mac_type = struct.unpack('! 6s 6s H', data[:14])
     mac_type = socket.htons(mac_type)
     return raw_dest_mac, raw_src_mac, mac_type, data[14:]
 
@@ -16,6 +16,12 @@ def protocol_identifier(type):
     proto_data = {8:'IPv4', 56710:'IPv6', 1544:'ARP', 13696:'Reverse ARP', 2184:'Ethernet Flow Control', 18568:'MPLS Multicast', 18312:'MPLS Unicast' }
     return proto_data[type]
 
+def ip_pckt(pckt):
+    ver_head_len = pckt[0]
+    version = ver_head_len >> 4
+    head_len = (ver_head_len & 15) * 4
+    ttl, ip_proto, raw_src_ip, raw_dest_ip = struct.unpack('! 8x B B 2x 4s 4s', pckt[:20])
+    return version, head_len, ttl. ip_proto, raw_dest_ip, raw_src_ip, data[head_len:]
 
 def main():
     connect = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
@@ -23,9 +29,8 @@ def main():
     while True:
         raw_data, address = connect.recvfrom(65536)
         raw_dest_mac, raw_src_mac, mac_type, pckt = mac_extracter(raw_data)
-        dest_mst_mac)
+        dest_mac = actual_mac(raw_dest_mac)
         src_mac = actual_mac(raw_src_mac)
         protocol = protocol_identifier(mac_type)
         print ('\nDestination MAC:{}\nSource MAC:{}\nProtocol:{}\n'.format(dest_mac, src_mac, protocol))
-        ip_header_parser(pckt)
 main()
